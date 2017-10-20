3c645c7e494381f133271b57bab6d691f049530d
Konstantin Chugalinskiy <kchugalinskiy@yandex.ru>
2017-10-21 01:37:44 +0300

    Moved forgotten submodules

--- examples/full/main.tf
+++ examples/full/main.tf
@@ -1,52 +1,52 @@
 module "assert-conditional" {
-    source = "kchugalinskiy/assert/null"
+  source    = "kchugalinskiy/assert/null"
   condition = "${1 != 2}"
 }
 
 module "assert-equality" {
-  source = "kchugalinskiy/assert/null//modules/eq?ref=v0.0.2"
+  source   = "git::git@github.com:kchugalinskiy/terraform-null-assert.git//modules/eq"
   expected = 1
   actual   = 1
 }
 
 module "assert-equality-strings" {
-  source = "kchugalinskiy/assert/null//modules/eq?ref=v0.0.2"
+  source   = "git::git@github.com:kchugalinskiy/terraform-null-assert.git//modules/eq"
   expected = "hello world"
   actual   = "hello world"
 }
 
 module "assert-unequality" {
-  source = "kchugalinskiy/assert/null//modules/neq?ref=v0.0.2"
+  source     = "git::git@github.com:kchugalinskiy/terraform-null-assert.git//modules/neq"
   unexpected = "hello world"
   actual     = "hello"
 }
 
 module "assert-greater-or-equal" {
-  source = "kchugalinskiy/assert/null//modules/ge?ref=v0.0.2"
+  source   = "git::git@github.com:kchugalinskiy/terraform-null-assert.git//modules/ge"
   expected = 0
   actual   = 2
 }
 
 module "assert-strict-greater" {
-  source = "kchugalinskiy/assert/null//modules/ge?ref=v0.0.2"
+  source   = "git::git@github.com:kchugalinskiy/terraform-null-assert.git//modules/gt"
   expected = 0
   actual   = 1
 }
 
 module "assert-less-or-equal" {
-  source = "kchugalinskiy/assert/null//modules/ge?ref=v0.0.2"
+  source   = "git::git@github.com:kchugalinskiy/terraform-null-assert.git//modules/le"
   expected = 2
   actual   = 0
 }
 
 module "assert-strict-less" {
-  source = "kchugalinskiy/assert/null//modules/ge?ref=v0.0.2"
+  source   = "git::git@github.com:kchugalinskiy/terraform-null-assert.git//modules/lt"
   expected = 1
   actual   = 0
 }
 
 module "assert-match-regex" {
-  source = "kchugalinskiy/assert/null//modules/match?ref=v0.0.2"
+  source = "git::git@github.com:kchugalinskiy/terraform-null-assert.git//modules/match"
   regex  = "[a-z]*"
   actual = "rego"
 }
